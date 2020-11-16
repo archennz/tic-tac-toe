d41c22f23379abd455e7ab35d65c98874a0e06cb
archennz <an.ran.chen1994@gmail.com>
2020-11-16 17:45:44 +1300

    following the tutorial

--- src/index.js
+++ src/index.js
@@ -2,24 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+  
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
     const status = 'Next player: X';
-
     return (
       <div>
         <div className="status">{status}</div>
@@ -44,19 +61,19 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+ render() {
+   return (
+     <div className="game">
+       <div className="game-board">
+         <Board />
+       </div>
+       <div className="game-info">
+         <div>{/* status */}</div>
+         <ol>{/* TODO */}</ol>
+       </div>
+     </div>
+   );
+ }
 }
 
 // ========================================
